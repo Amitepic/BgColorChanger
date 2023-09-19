909b5534a747adafc2619184b7c6e606587251c8
Amitepic <129495075+Amitepic@users.noreply.github.com>
2023-09-19 23:17:37 +0530

    Add files via upload

---
+++ chai.js
@@ -0,0 +1,25 @@
+const box = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+
+box.forEach( function (button){
+    button.addEventListener('click',function (e){
+        // console.log(e)
+    //    console.log(e.target);
+       if(e.target.id === 'grey'){
+         body.style.backgroundColor = e.target.id
+       }
+       if(e.target.id === 'white'){
+        body.style.backgroundColor = e.target.id
+      }
+      if(e.target.id === 'purple'){
+        body.style.backgroundColor = e.target.id
+      }
+      if(e.target.id === 'yellow'){
+        body.style.backgroundColor = e.target.id
+      }
+      if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id
+      }
+    })
+})
